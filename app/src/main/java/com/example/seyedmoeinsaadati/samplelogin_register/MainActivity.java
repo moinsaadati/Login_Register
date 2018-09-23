8a0c342a0bff8f21ded84fb3563a5c077cf30e26
Seyed Moein Saadati <Saadati.moin@gmail.com>
2018-09-24 00:22:08 +0330

    create register fragment (ui)

--- app/src/main/java/com/example/seyedmoeinsaadati/samplelogin_register/MainActivity.java
+++ app/src/main/java/com/example/seyedmoeinsaadati/samplelogin_register/MainActivity.java
@@ -14,16 +14,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.fragment_login);
+        setContentView(R.layout.fragment_register);
         hideStatusBar();
-
-        findViewById(R.id.btn_login).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(MainActivity.this, "", Toast.LENGTH_SHORT).show();
-            }
-        });
-
     }
 
     private void hideStatusBar() {
@@ -32,6 +24,4 @@ public class MainActivity extends AppCompatActivity {
         decorView.setSystemUiVisibility(uiOptions);
         getSupportActionBar().hide();
     }
-
-
 }
---
+++ app/src/main/java/com/example/seyedmoeinsaadati/samplelogin_register/RegisterFragment.java
@@ -0,0 +1,25 @@
+package com.example.seyedmoeinsaadati.samplelogin_register;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class RegisterFragment extends Fragment {
+
+
+    public RegisterFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.fragment_register, container, false);
+    }
+
+}
